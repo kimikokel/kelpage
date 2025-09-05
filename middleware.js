f97d8285fc66eb482a5509f50ff1959ebe9eb440
kel <kellyleong2002@gmail.com>
2025-09-05 09:58:38 +0800

    update middleware.js to redirect static pages

---
+++ middleware.js
@@ -0,0 +1,12 @@
+// middleware.js
+export const config = {
+  matcher: ['/bookstore/:path*', '/macaucalendar/:path*']
+};
+
+export default function middleware(request) {
+  const url = new URL(request.url);
+  const amplifyDomain = 'https://main.d7qiai7u4ok60.amplifyapp.com';
+  
+  // Rewrite to Amplify
+  return Response.rewrite(`${amplifyDomain}${url.pathname}`);
+}
